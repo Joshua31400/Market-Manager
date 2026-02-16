ca963eb7e5f1d28d303df55a248b8dbd353b8b5f
bjoshua <joshua.budgen@ynov.com>
2026-02-16 11:17:50 +0100

    feat(HelpCmd.java): Add new command - Staff and admin command to administrate user, rules and stock

--- src/commands/handlers/common/HelpCmd.java
+++ src/commands/handlers/common/HelpCmd.java
@@ -27,6 +27,8 @@ public class HelpCmd extends CommandHandler {
         return """
                Common commands:
                - help: Show this help message.
+               - catalog - List all items in the catalog.
+               - info <id> - Show information about an item by its ID.
                - clear: Clear the terminal.
                - quit: Exit the application.
                """;
@@ -35,12 +37,28 @@ public class HelpCmd extends CommandHandler {
     private String getClientHelpMessage() {
         return """
                Available commands:
-                - catalog - List all items in the catalog.
-                - info <id> - Show information about an item by its ID.
                 - add <id> - Add item to cart by id.
                 - remove <id> - Remove item from cart by id.
                 - cart - View items in your shopping cart.
                 - buy - Purchase items in your cart.
+               """;
+    }
+
+    private String getStaffHelpMessage() {
+        return """
+               Available commands:
+                - additem <name> <price> <quantity> - Add a new item to the catalog.
+                - removeitem <id> - Remove an item from the catalog by its ID.
+               """;
+    }
+
+    private String getAdminHelpMessage() {
+        return """
+               Available commands:
+                - userlist - List all users in the system.
+                - adduser <username> <password> <permission> - Add a new user with the specified username, password, and permission level (CLIENT, STAFF, ADMIN).
+                - updatepermission <id> <status> - Update the permission of an user by its ID. Status can be CLIENT, STAFF or ADMIN.
+                - removeuser <id> - Remove a user by their username.
                """;
     }
 
